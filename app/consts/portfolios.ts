16581faaeb61cadd8a1a793b9eaeea820520c42a
Syaina Nur <syaina@femaledaily.com>
2023-12-27 22:58:14 +0700

    feat: add portfolios const

---
+++ app/consts/portfolios.ts
@@ -0,0 +1,30 @@
+export const portfolios = [
+  {
+    id: 1,
+    title: "Aku Maju (2022)",
+    description:
+      "Aku Maju is an application for HR department to help in administration. We build this project with <b>Typescript</b> and <b>NextJS </b> in Frontend side and <b>Strapi</b> in Backend side (this project requires to do CMS). This project contains of monorepo concept to run. I collaborated with Firman in this project.",
+    image: "/images/portfolio/akumaju.png",
+  },
+  {
+    id: 2,
+    title: "Tentir Aorta (2021)",
+    description:
+      "Tentir Aorta is a website for medical student in Universitas Padjadjaran to enroll classes for mentoring session with mentors which provided from Tentir Aorta. This project also provide a quiz feature which can grade user’s answer with exact time. Build with React for Frontend side and Lumen for Backend side. This website also contains of responsive design. I worked by myself for developed this whole project as a fullstack developer from design until deployment.",
+    image: "/images/portfolio/tentiraorta.png",
+  },
+  {
+    id: 3,
+    title: "Loakarya (2021)",
+    description:
+      "Loakarya is a company which raised by Universitas Padjadjaran student in order to utilize building material waste to be a new product which had IoT system. This project build to create a landing page for Loakarya, articles page, and a sell product page. This website build with React for Frontend side and Lumen for Backend side. I collaborated with Fikri as a backend.",
+    image: "/images/portfolio/loakarya.png",
+  },
+  {
+    id: 4,
+    title: "Funder.id (2020)",
+    description:
+      "Funder.id is a real time platform for equity crowd funding which user can invest in a small amount, start from Rp 50.000. This project has verification system for email to prevent fake account. Another feature also developed in this project such as e-wallet and webinar e-form. Build with Laravel for Frontend side, I collaborated with David who provide API with Lumen, and Agnes who provide the design. This website has reached 100+ user in a month after launched.",
+    image: "/images/portfolio/funder.png",
+  },
+] as const;
