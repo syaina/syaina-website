ee0e6057763957055a416c7067a03ee66701a5ff
Syaina Nur <syaina@femaledaily.com>
2023-12-27 21:51:38 +0700

    feat: add personal section and portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,201 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import cx from "classnames";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <main className={styles.main}>
+        <h1 className="heading-1 color-primary">
+          Hi! I am Syaina, <br />3 years experienced Front End Developer.
+        </h1>
+        <div className="main-img-container" style={{ position: "relative" }}>
+          <Image
+            src={"/images/main/main-1.png"}
+            alt="Main 1 Image"
+            width={560}
+            height={457}
+            quality={100}
+          />
+        </div>
+      </main>
+
+      <div id="about-me" className={styles.aboutMeSection}>
+        <div style={{ position: "relative" }}>
+          <Image
+            src={"/images/main/main-2.png"}
+            alt="Main 2 Image"
+            width={503}
+            height={382}
+            quality={100}
+          />
+        </div>
+        <div className={styles.aboutMeContent}>
+          <h2 className="heading-2 color-tertiary-secondary">Who I am</h2>
+          <p className="text-secondary color-secondary">
+            With over 2+ years of experience as a full-time Front End Developer,
+            I have honed my skills in Typescript, with a focus on leveraging
+            NextJS to develop features that prioritize user experience while
+            minimizing page load times. My approach emphasizes generating
+            reusable, readable code or components. Additionally, I possess a
+            meticulous nature, which drives me to analyze and plan before
+            embarking on any development.
+          </p>
+
+          <h3 className={styles.h3}>AQUIRED SKILL</h3>
+          <div className={styles.skillContainer}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src={
+                "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              }
+              alt="Javascript"
             />
-          </a>
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src={
+                "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+              }
+              alt="Typescript"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src={
+                "https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-1024.png"
+              }
+              alt="Python"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="https://laravel.com/img/logomark.min.svg"
+              alt="Laravel"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="HTML5"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="CSS3"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
+              alt="Sass"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+              alt="NextJS"
+            />
+            <Image
+              width={40}
+              height={40}
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="ReactJS"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="/images/social-media/react-query-logo.png"
+              alt="React Query"
+            />
+            <Image
+              width={40}
+              height={40}
+              src="/images/social-media/sonarqube-logo.png"
+              alt="SonarQube"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="/images/social-media/ant-design-logo.svg"
+              alt="Ant Design"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="/images/social-media/sentry-logo.png"
+              alt="Sentry"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="/images/social-media/jest-logo.png"
+              alt="Jest"
+            />
+            <Image
+              className={styles.skillItem}
+              width={40}
+              height={40}
+              src="/images/social-media/rtl-logo.png"
+              alt="React Testing Library"
+            />
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        id="experience"
+        className={`${styles.sectionContainer} ${styles.experienceContainer}`}
+      >
+        {/* ... */}
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div
+        id="portfolio"
+        className={`${styles.sectionContainer} ${styles.portfolio}`}
+      >
+        <h2 className="heading-2 color-tertiary-secondary">PORTFOLIO</h2>
+        <div className={`${styles.portfolioBtnWrapper}`}>
+          <button
+            className={`${cx(
+              styles.portfolioBtn,
+              styles.active
+            )} text-secondary color-secondary`}
+          >
+            All
+          </button>
+          <button
+            className={`${cx(
+              styles.portfolioBtn
+              // styles.active
+            )} text-secondary color-secondary`}
+          >
+            Development
+          </button>
+          <button
+            className={`${cx(
+              styles.portfolioBtn
+              // styles.active
+            )} text-secondary color-secondary`}
+          >
+            Design
+          </button>
+        </div>
       </div>
-    </main>
-  )
+    </>
+  );
 }
