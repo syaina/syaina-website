220d22224093f92df9344f444af1bc151d575021
Syaina Nur <syaina@femaledaily.com>
2023-12-28 00:45:38 +0700

    feat: add contacts const

---
+++ app/_consts/contacts.ts
@@ -0,0 +1,23 @@
+export const contacts = [
+  {
+    id: "contact-1",
+    title: "Email",
+    icon: "/images/social-media/email.svg",
+    value: "syainanurf@gmail.com",
+    link: "mailto:syainanurf@gmail.com",
+  },
+  {
+    id: "contact-2",
+    title: "Linkedin",
+    icon: "/images/social-media/linkedin.svg",
+    value: "linkedin.com/in/syainanurf",
+    link: "https://linkedin.com/in/syainanurf",
+  },
+  {
+    id: "contact-3",
+    title: "Github",
+    icon: "/images/social-media/github.svg",
+    value: "github.com/syaina",
+    link: "https://github.com/syaina",
+  },
+] as const;
