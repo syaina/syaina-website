41104b2fd24865408c38f85a393822aaab9f2cc9
Syaina Nur <syaina@femaledaily.com>
2024-04-13 00:53:04 +0700

    feat: hide blog on navbar

--- app/_components/navbar/index.tsx
+++ app/_components/navbar/index.tsx
@@ -34,7 +34,7 @@ export const Navbar = () => {
         >
           Portfolio
         </button>
-        <button
+        {/* <button
           className="text-secondary"
           onClick={scrollTo.bind(null, {
             id: "blog",
@@ -42,7 +42,7 @@ export const Navbar = () => {
           })}
         >
           Blog
-        </button>
+        </button> */}
         <button
           className="text-secondary"
           onClick={scrollTo.bind(null, {
